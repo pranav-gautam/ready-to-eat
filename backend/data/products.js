2ee46d3d88f282f9a757a079b14b71980632e151
Pranav Gautam <pranavgautam27@gmail.com>
2022-09-08 12:17:39 -0400

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: 'Aloo Paratha',
+    image: '/images/aloo_paratha.png',
+    description:
+      'Aloo Paratha is a bread dish originating from the Indian subcontinent. It is a breakfast dish originated from the Punjab region.',
+    price: 40,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Burger',
+    image: '/images/burger.png',
+    description:
+      'A hamburger is a food, typically considered a sandwich, consisting of one or more cooked patties',
+    price: 40,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Chole Bhature',
+    image: '/images/chole_bhature.png',
+    description:
+      'Chole bhature is a food dish popular in the Northern areas of the Indian subcontinent. It is a combination of chana masala and bhatura/puri, a fried bread made from maida.',
+    price: 60,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Coffee',
+    image: '/images/coffee.png',
+    description:
+      "It ain't Tea mate init",
+    price: 10,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Dosa',
+    image: '/images/dosa.png',
+    description:
+      'A dosa is a thin pancake or crepe originating from South India, made from a fermented batter predominantly consisting of lentils and rice.',
+    price: 40,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Samosa',
+    image: '/images/samosa.png',
+    description:
+      'A samosa is a South Asian fried or baked pastry with a savory filling like spiced potatoes, onions, peas, chicken and other meats, or lentils.',
+    price: 20,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
