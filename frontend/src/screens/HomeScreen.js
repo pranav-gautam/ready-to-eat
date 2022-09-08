46ba5de49baecbc8bc03bff4bbadc368158a2075
Pranav Gautam <pranavgautam27@gmail.com>
2022-09-08 13:13:48 -0400

    Add files via upload

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import ProductCarousel from '../components/ProductCarousel'
+
+
+const HomeScreen = ({ match }) => {
+
+    const keyword = match.params.keyword
+    const dispatch = useDispatch();
+
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
+    useEffect(() => {
+        dispatch(listProducts(keyword))
+    }, [dispatch, keyword])
+
+
+    return (
+        <>
+            {!keyword ? (
+                <ProductCarousel />
+            ) : (
+                <Link to='/' className='btn btn-light'>
+                    Go Back
+                </Link>
+            )}
+            <h1>Menu</h1>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <h2><Message variant='danger'>{error}</Message></h2>
+            ) : (
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            )
+            }
+
+        </>
+    )
+}
+
+export default HomeScreen
